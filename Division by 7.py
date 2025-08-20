1331c0120871fa378b8d64932ab658f9ce8f69d5
amankushwahaa2888 <amankushwaha1777@gmail.com>
2025-08-20 14:24:58 +0530

    Add files via upload

---
+++ Division by 7.py
@@ -0,0 +1,12 @@
+"""a=int(input("Enter the number:"))
+if a%7==0:
+    print("Division by 7 is:",a)
+else :
+    print("NOT")"""
+
+
+a=int(input("Enter the number:"))
+lastdigit=(repr(a)[-5:-1])
+print(lastdigit)
+
+
---
+++ last 4 digit.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number:"))
+if a%4==0:
+    print("THIS LAST DIGIT 4")
+else:
+    print("THIS IS NOT LAST DIGIT 4")
+
+
+
