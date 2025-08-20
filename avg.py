f19265c924a2adc74f760d8d9b846fcd11480aeb
amankushwahaa2888 <amankushwaha1777@gmail.com>
2025-08-20 19:13:31 +0530

    Update avg.py

--- avg.py
+++ avg.py
@@ -1,4 +1,3 @@
-#11. Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers. 
 A=int(input("Enter A number"))
 B=int(input("Enter B number"))
 C=int(input("Enter C number"))
@@ -6,3 +5,4 @@ D=int(input("Enter D number"))
 E=int(input("Enter E number"))
 avg=(A+B+C+D+E)/5
 print("Average:",avg)
+
