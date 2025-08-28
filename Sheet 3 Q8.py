ca36a0a5d8e0c12dc0dad4845f5da883c55c4db3
amankushwahaa2888 <amankushwaha1777@gmail.com>
2025-08-28 20:29:36 +0530

    Add files via upload

---
+++ Sheet 3 Q8.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1): 
+    for j in range(n - i):
+        print("_", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
